a1096af911d755c323a3a227752de5fc51047cbe
ArshaanKhurana <74656444+ArshaanKhurana@users.noreply.github.com>
2021-09-10 17:18:37 +0530

    Add files via upload

---
+++ js/levelScreen.js
@@ -0,0 +1,35 @@
+class Levels{
+    constructor(x,y,lvlName){
+      this.levels = createSprite(x,y)
+      this.levels.addImage(CloseLockImg)
+      this.levels.scale = 0.5;
+      this.levels.visible = false;
+
+      this.lvl = lvlName
+      this.posX=x;
+      this.posY=y;
+    }
+
+    hide(){
+      this.levels.destroy(); 
+     }
+
+    display(){
+        background("yellow");
+        Hero.hide()
+        this.levels.visible = true;
+        
+        textSize(50);
+        text("Level "+this.lvl,this.posX,-50+this.posY)
+
+        if(this.lvl<=playedLvls){
+        this.levels.addImage(OpenLockImg)
+        if(mousePressedOver(this.levels)){
+          gameState = 2;
+          funObj.gameStateUpdate(2);
+      }        
+        }
+        
+    }
+
+}
---
+++ js/mainScreen.js
@@ -0,0 +1,35 @@
+class MainScreen {
+   constructor(x,y){
+       console.log("Hi")
+       this.hero=createSprite(x,y)
+       this.hero.addImage(SuperHeroimg)
+       this.hero.scale=1.5
+       
+       this.arrow = createSprite(775,425)
+       this.arrow.addImage(arrowImg);
+       this.arrow.scale=0.3;
+   }
+
+    hide(){
+     this.hero.visible=false 
+     this.arrow.destroy();
+    }
+
+    display(){
+        background(mainMenuImg);
+        fill("Red")
+        stroke("Black")
+        strokeWeight(10)
+        textSize(35)
+        text("SUPER NOVA - THE PROTECTOR OF UNIVERSE",100,230)
+
+        if(mousePressedOver(this.arrow)){
+            gameState = 1;
+            funObj.gameStateUpdate(1)
+            funObj.levelUpdate(1)
+        }
+    }
+}
+
+
+
